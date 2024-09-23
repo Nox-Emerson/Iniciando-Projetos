ca44a90f2d23a5342504ae46c1d1bce039c0fd7c
Emerson <etielemerson@outlook.com>
2024-09-23 19:49:11 -0300

    fim da aula 2 do proj. Py

--- Projeto Python/app.py
+++ Projeto Python/app.py
@@ -1,7 +1,47 @@
 # importação
-from flask import Flask
+from flask import Flask,request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+# ORM - Object Relation Mapper (Objeto de Relação Mapeador), gera uma camada de abstração para o Banco de Dados. O código reage com o ORM e o ORM interage com o Banco de Dados.
 
 app = Flask(__name__) #instancia a variável Flask
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///ecommerce.db"
+db = SQLAlchemy(app)
+
+#Modelagem 
+# Produto (id, name, price, description)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+
+@app.route('/api/products/add', methods=["POST"]) #alt
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description",""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product Added Sucessfully"})
+    return jsonify({"message": "Invalid Product Data"}), 400
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    #Recuperar o produto da base de dados
+    product = Product.query.get(product_id)
+    #Verificar se o produto existe
+    if product != None:
+    #Se existe, apagar da base de dados
+        db.session.delete(product)
+        db.session.commit()
+    #Se não existe, retornar 404 not found
+        return jsonify({"message": "Product Deleted Sucessfully"})
+    return jsonify({"message": "Product Not Found"}), 404
+
+
 
 # Rota raiz (da pag. inicial) e a função que será executada ao requisitar.
 @app.route('/')
